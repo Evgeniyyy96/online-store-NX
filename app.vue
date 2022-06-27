a34507c09e563daad412e7d9c3b692058b1dd6ba
Evgeniyyy96 <evgeniytovstokoriy96@gmail.com>
2022-06-27 11:05:45 +0300

    load

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container mb-3">
+     <TheNavbar />
+    <NuxtPage />
+  </div>
+</template>
+<script>
+import TheNavbar from "./components/TheNavbar";
+import ShoppingCart from "./components/ShoppingCart";
+export default {
+  components: {ShoppingCart, TheNavbar}
+}
+</script>
